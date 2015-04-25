b22645531e69900546c1caa1a7fa7e3d364ba44d
Tommaso Elli <tommasoelli@Tommasos-MBP-001.homenet.telecomitalia.it>
2015-04-26 01:19:11 +0200

    random opacity js

---
+++ Random Objects Opacity/RandomOpacity.js
@@ -0,0 +1,24 @@
+﻿mySelection = app.activeDocument.selection;
+myDoc = app.activeDocument;
+if (mySelection instanceof Array)
+{
+	selOpacity = [0,10,20,30,40,50,60,70,80,90,100];
+		
+	if(selOpacity.length != 0)
+		for (i=0; i<mySelection.length; i++)
+		{
+			if(mySelection[i].typename == "PathItem" || mySelection[i].typename == "CompoundPathItem")
+			{
+				selItem = mySelection[i];
+				selItem.filled = true;
+
+				swatchIndex = Math.round( Math.random() * (selOpacity.length - 1 ));
+				
+				if(selItem.typename == "PathItem")
+					selItem.opacity = selOpacity[swatchIndex];
+				else
+					selItem.pathItems[0].opacity = selOpacity[swatchIndex];
+				
+			}
+		}
+}
