bcd87267e321fd51a8d2a33c47f1c2708b5adc21
iosonosempreio <tommaso.elli@gmail.com>
2015-05-27 09:14:08 +0200

    addStroke

---
+++ Add Stroke from Fill/add Stroke.md
@@ -0,0 +1,12 @@
+# Random Object Opacity
+
+Copy this JS file to the script folder of Illustrator, which it should be similar to...
+
+*Adobe Illustrator XX / Presets / en_US / Scripts*
+
+Then:
+* Restart AI
+* Select the objects you want to edit
+* Go to File > Scripts > addStroke.js
+
+Now you should see that your objects stroke took the same color of their fill.
---
+++ Add Stroke from Fill/addStroke.js
@@ -0,0 +1,26 @@
+﻿mySelection = app.activeDocument.selection;
+myDoc = app.activeDocument;
+if (mySelection instanceof Array)
+{
+	for (i=0; i<mySelection.length; i++) {
+		selItem = mySelection[i];
+		if (selItem.filled === true) {
+			if(mySelection[i].typename == "PathItem" || mySelection[i].typename == "CompoundPathItem"){			
+				selItem = mySelection[i];
+				var color = selItem.fillColor;
+				
+				if(selItem.typename == "PathItem"){
+					selItem.stroked = true;				
+					selItem.strokeColor = selItem.fillColor;
+					selItem.strokeWidth = 2;		
+				}				
+				else {
+					selItem.pathItems[0].stroked = true;
+					
+					selItem.pathItems[0].strokeColor = selItem.fillColor;
+					selItem.pathItems[0].strokeWidth = 2;
+				}
+			}
+		}	
+	}	
+}
